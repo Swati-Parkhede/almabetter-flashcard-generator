35f89a45e81c61251e0f24c835160d34b374f80d
swati parkhede <swatiparkhede@gmail.com>
2025-09-28 17:27:48 +0530

    implemented Add flash card functionality

--- src/components/EditFlashCardItemList/EditFlashCardItemList.jsx
+++ src/components/EditFlashCardItemList/EditFlashCardItemList.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./EditFlashCardItemList.css"
 import EditFlashCardItem from '../EditFlashCardItem/EditFlashCardItem'
 
-export const EditFlashCardItemList = () => {  
-  return (
-    <div>
-        <EditFlashCardItem cardItemIndex="1"/>  
-    </div>
-  )
+export const EditFlashCardItemList = () => {
+    const [flashcards, setFlashcards] = useState([1])
+
+    const handleOnClick = () => {
+        flashcards.push(flashcards.length + 1)
+        setFlashcards([...flashcards])
+    };
+
+    return (
+        <div>
+            <div>
+                {
+                    flashcards.map((fc) =>
+                        <EditFlashCardItem key={fc} cardItemIndex={fc} />
+                    )
+                }
+            </div>
+            <button className='AddCardItemBtn' onClick={handleOnClick}> + Add more</button>
+        </div>
+    )
 }
